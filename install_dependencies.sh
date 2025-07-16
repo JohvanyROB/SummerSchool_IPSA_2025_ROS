4ca2ebac552dac32377570867254bb059d9361d7
Johvany Gustave <32563846+JohvanyROB@users.noreply.github.com>
2025-07-16 10:09:04 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,6 +9,7 @@ sudo apt -y install cppzmq-dev  #For ROS Development Studio
 source /opt/ros/jazzy/setup.bash
 sudo apt -y install ros-${ROS_DISTRO}-ros-gz ros-${ROS_DISTRO}-tf-transformations
 
+source /opt/ros/jazzy/setup.bash
 export PYTHONWARNINGS="ignore:easy_install command is deprecated,ignore:setup.py install is deprecated" #For ROS Development Studio
 cd ~/ros2_ws && colcon build --symlink-install
 echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
